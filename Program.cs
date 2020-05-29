e58d2ea9704c573b993e49cc77b792e21ff675a8
diego_neri <diego@diegoneri.com>
2020-05-29 20:11:35 -0300

    Adiciona o exercício parcialmente solucionado, restando alguns termos da lista a serem substituídos

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+[console: CriancaEducada] Solicite que o usuário digite uma frase. Substitua as palavras da lista por uma sequência de símbolos (como #@$%*!&, por exemplo).
+chato/chata, bobo/boba, feio/feia, boboca, bocó, tonto/tonta, palerma, paspalho/paspalha, tantã, panaca, pentelho/pentelha, burro/burra, besta
+*/
+
+namespace CriancaEducada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            string fraseFinal;
+            
+            Console.WriteLine("Me diga o que você pensa sobre o aluno que não faz a lição de casa:");
+
+            frase = Console.ReadLine();
+
+            fraseFinal = frase.ToLower()
+                              .Replace("chato", "#@$%*!&")
+                              .Replace("chata", "#@$%*!&")
+                              .Replace("bobo", "#@$%*!&")
+                              .Replace("boba", "#@$%*!&"); 
+
+            Console.WriteLine(fraseFinal); 
+
+        }
+    }
+}
